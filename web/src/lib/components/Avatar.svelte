66886d88b5723e84136920b3425939190bc22a3a
jonasbg <1508560+jonasbg@users.noreply.github.com>
2024-01-08 09:53:23 +0000

    fix(avatar): add user info overlay

--- web/src/lib/components/Avatar.svelte
+++ web/src/lib/components/Avatar.svelte
@@ -41,7 +41,7 @@
       <div class="row row-0">
         <div class="col-2">
           {#if user}
-          <img src={user.Picture} class="w-100 h-100 object-cover card-img-start" alt={user.Name}>
+          <img src={user.Picture} class="avatar avatar-md object-cover card-img-start m-1" alt={user.Name}>
           {:else}
               <svg xmlns="http://www.w3.org/2000/svg" class="w-100 h-100 object-cover card-img-start icon icon-tabler icon-tabler-user-square" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 10a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" /><path d="M6 21v-1a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v1" /><path d="M3 5a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-14z" /></svg>
           {/if}
