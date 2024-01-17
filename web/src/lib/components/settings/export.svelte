21d0e5c9fff6d719955696776336233d02e1466b
jonasbg <1508560+jonasbg@users.noreply.github.com>
2024-01-17 11:33:46 +0000

    settings color fix

--- web/src/lib/components/settings/export.svelte
+++ web/src/lib/components/settings/export.svelte
@@ -64,7 +64,8 @@
 </script>
 <div class="card-body">
   <h2 class="card-title">Export data</h2>
-  <p class="text-secondary">Export the entire database file. Importing this file will overwrite any existing database file and terminate any active connections. All users will be <strong>logged out.</strong> Please exercise <strong >CAUTION</strong> when using this option. <span class="text-yellow bg-transparent"> The server will perform a hard restart after the import.</span></p>
+  <p class="text-secondary">Export the entire database file. Importing this file will overwrite any existing database file and terminate any active connections. All users will be <strong>logged out.</strong> Please exercise <strong >CAUTION</strong> when using this option. </p>
+  <span class="badge bg-orange-lt mb-3">The server will perform a hard restart after the import</span>
   <div class="btn-list justify-content-end">
     <input type="file" id="file-input" bind:this={fileInput} on:change={handleFileChange} hidden>
     <button on:click={uploadFile} class="btn btn-outline-danger">Import Data</button>
