c54ba812abae5fbec294fdf5b032281d2ac3491f
jonasbg <1508560+jonasbg@users.noreply.github.com>
2024-01-18 09:48:52 +0000

    bug(event): if vulnerability is deleted, do not try to process

--- api/event/eventQueue.go
+++ api/event/eventQueue.go
@@ -110,7 +110,12 @@ func handleEvent(event database.EventQueue) {
 	data.URL = appConfig.Cors.Origin + "/vulnerability/" + strconv.FormatUint(uint64(finding.ID), 10) + "/view"
 	var imageUrl string
 
-	userID, _ := findUserIDByEmail(finding.FoundBy)
+	userID, err = findUserIDByEmail(finding.FoundBy)
+
+	if err != nil {
+		log.Printf("User not found: %v", err)
+		return
+	}
 
 	if userID == "" {
 		data.FoundBy = finding.FoundBy
