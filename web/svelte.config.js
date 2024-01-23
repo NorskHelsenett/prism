5b700c329c64e2642338a0a86df8e4d1dd44fe2d
Jonas Bo Grimsgaard <jonas.bo.grimsgaard@nhn.no>
2024-01-23 13:44:40 +0000

    add(CSP): added CSP for the solution when in production build only

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,15 +1,21 @@
 import adapter from '@sveltejs/adapter-static';
 import cspDirectives from './csp-directives.mjs';
+const production = process.env.NODE_ENV === 'production';
 
 export default {
 	kit: {
 		adapter: adapter({
 			fallback: 'index.html' // may differ from host to host
 		}),
-		csp: {
-			mode: "hash",
-			directives: cspDirectives
-		},
+		...(production ? {
+			// Production only configuration
+			csp: {
+				mode: "hash",
+				directives: cspDirectives
+			},
+		} : {
+			// Development only configuration
+		})
 	}
 };
 
