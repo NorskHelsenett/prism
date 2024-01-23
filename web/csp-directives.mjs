a12cdae273bac488f55d19be365417a254813fbb
Jonas Bo Grimsgaard <jonas.bo.grimsgaard@nhn.no>
2024-01-23 13:03:14 +0000

    fix(script): removed unsafe-inline directive

--- web/csp-directives.mjs
+++ web/csp-directives.mjs
@@ -3,7 +3,7 @@ const cspDirectives = {
   'child-src': ["'self'"],
   'connect-src': ["'self'", 'ws://localhost:*'],
   // 'connect-src': ["'self'", 'ws://localhost:*', 'https://hcaptcha.com', 'https://*.hcaptcha.com'],
-  'img-src': ["'self'", "https://helsegitlab.nhn.no","https://secure.gravatar.com/avatar", 'data:'],
+  'img-src': ["'self'", "https://helsegitlab.nhn.no","https://secure.gravatar.com", 'data:'],
   'font-src': ["'self'", 'data:'],
   'form-action': ["'self'"],
   'frame-ancestors': ["'self'"],
