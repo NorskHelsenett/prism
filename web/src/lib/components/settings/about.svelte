251d2534ccf6804ba5cb6b7dfbb7a2f0f2b53281
Jonas Bo Grimsgaard <jonas.bo.grimsgaard@nhn.no>
2024-01-30 19:16:49 +0000

    fix(about): total code lines

--- web/src/lib/components/settings/about.svelte
+++ web/src/lib/components/settings/about.svelte
@@ -2,12 +2,14 @@
   import { onMount } from 'svelte';
   import { marked } from 'marked';
   import DOMPurify from 'dompurify'
+	import formatNumber from '$lib/formatNumber';
 
   let buildInfo = {
     "user": "",
     "branch": "",
     "date": "",
-    "digest": ""
+    "digest": "",
+    "totalLines": 0
   }
   let changelog = '';
 
@@ -51,6 +53,13 @@
       <div class="datagrid-content">
       <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-git-branch" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 18m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M7 6m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M17 6m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M7 8l0 8" /><path d="M9 18h6a2 2 0 0 0 2 -2v-5" /><path d="M14 14l3 -3l3 3" /></svg>
         {buildInfo.branch}
+      </div>
+    </div>
+    <div class="datagrid-item">
+      <div class="datagrid-title">Code Lines</div>
+      <div class="datagrid-content">
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-description" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 3v4a1 1 0 0 0 1 1h4" /><path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" /><path d="M9 17h6" /><path d="M9 13h6" /></svg>
+        {formatNumber(buildInfo.totalLines)}
       </div>
     </div>
   </div>
