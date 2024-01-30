6ab95eb51fb7946e0791fd7546648a8b4c8e3735
Jonas Bo Grimsgaard <jonas.bo.grimsgaard@nhn.no>
2024-01-30 20:06:56 +0000

    fix(users): add last seen row to user table

--- web/src/lib/components/settings/UserTable.svelte
+++ web/src/lib/components/settings/UserTable.svelte
@@ -36,6 +36,37 @@ function formatDate(dateString) {
   const date = new Date(dateString);
   return date.toLocaleDateString('en-US', options);
 }
+
+  function formatDateText(expiresAt) {
+    const now = new Date();
+    const expiry = new Date(expiresAt);
+    let difference = expiry - now;
+
+    const isInPast = difference < 0;
+    difference = Math.abs(difference);
+
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    difference -= days * (1000 * 60 * 60 * 24);
+
+    const hours = Math.floor(difference / (1000 * 60 * 60));
+    difference -= hours * (1000 * 60 * 60);
+
+    const minutes = Math.floor(difference / (1000 * 60));
+
+    let formattedDate = "";
+    if (days > 0) formattedDate += `${days} day${days > 1 ? 's' : ''} `;
+    if (hours > 0) formattedDate += `${hours} hour${hours > 1 ? 's' : ''} `;
+    if (minutes > 0) formattedDate += `${minutes} minute${minutes > 1 ? 's' : ''}`;
+
+    // Add a prefix based on whether the date is in the past or future
+    if (isInPast) {
+        formattedDate = formattedDate ? `${formattedDate} ago` : "Already expired";
+    } else {
+        formattedDate = formattedDate ? `In ${formattedDate}` : "Now";
+    }
+
+    return formattedDate.trim();
+  }
 </script>
 
 <div class="card me-3 mb-3 mt-3">
@@ -45,6 +76,7 @@ function formatDate(dateString) {
         <tr>
           <th>Name</th>
           <th>Created</th>
+          <th>Last seen</th>
           <th>Role</th>
           <th class="w-1"></th>
         </tr>
@@ -61,7 +93,8 @@ function formatDate(dateString) {
               </div>
             </div>
           </td>
-          <td class="text-secondary">{formatDate(user.CreatedAt)}</td>
+          <td class="text-secondary" title={formatDateText(user.CreatedAt)}>{formatDate(user.CreatedAt)}</td>
+          <td class="text-secondary" title={formatDateText(user.UpdatedAt)}>{formatDate(user.UpdatedAt)}</td>
           <td data-label="Role">
             <select class="form-select" disabled={userIsAdmin(user.Role)} on:change={updateUser(user)}>
               {#each roles as role}
