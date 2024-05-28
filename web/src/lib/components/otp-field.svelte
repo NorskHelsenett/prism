dcdb3de0093221cad91a51b8f8c740b9f72d00b7
Jonas Bo Grimsgaard <jonas.bo.grimsgaard@nhn.no>
2024-05-28 08:43:39 +0000

    fix(otp): fixes OTP login field

--- web/src/lib/components/otp-field.svelte
+++ web/src/lib/components/otp-field.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { Fetch } from '$lib/fetchUtil';
+  import { Fetch } from '$lib/fetchUtil';
   import { onMount } from 'svelte';
 
   let otpInputs = [];
@@ -12,94 +12,100 @@
           if (index > 0) {
               otpInputs[index - 1].focus();
           }
-      } else if (otpCode[index].length === 1 && index < otpLength - 1) {
+      } else if (otpCode[index] && index < otpLength - 1) {
           otpInputs[index + 1].focus();
       }
   };
 
   onMount(() => {
-    if (otpInputs[0]) {
-      otpInputs[0].focus();
-    }
+      if (otpInputs[0]) {
+          otpInputs[0].focus();
+      }
   });
 
   const submitOTP = async () => {
-    const otp = otpCode.join('');
-    if (otp.length === otpLength && !submitting) {
-        // Add fetch API call here
-        submitting = true
-        try{
-          const response = await Fetch("/api/session/otp/validate", {method: "POST", body: JSON.stringify({"otp_code": otp})})
-          if (!response.error) {
-            window.location.href = '/';
+      const otp = otpCode.join('');
+      if (otp.length === otpLength && !submitting) {
+          // Add fetch API call here
+          submitting = true
+          try {
+              const response = await Fetch("/api/session/otp/validate", {
+                  method: "POST",
+                  body: JSON.stringify({ "otp_code": otp })
+              });
+              if (!response.error) {
+                  window.location.href = '/';
+              }
+          } catch {
+              // Handle error
+
+          } finally {
+              submitting = false
+              otpCode = Array(otpLength).fill('');
+              setTimeout(() => {
+                otpInputs[0].focus();
+              }, 10);
           }
-          setTimeout(() => {
-            submitting = false
-            otpCode = Array(otpLength).fill('');
-          }, 300);
-        } catch {
-          submitting = false
-        }
-    }
+      }
   };
 
-    const handlePaste = (event) => {
-        const paste = event.clipboardData.getData('text');
-        const numbers = paste.split('').filter(char => !isNaN(char) && char !== ' ').slice(0, otpLength);
+  const handlePaste = (event) => {
+      const paste = event.clipboardData.getData('text');
+      const numbers = paste.split('').filter(char => !isNaN(char) && char !== ' ');
 
-        if (numbers.length === otpLength) {
-            otpCode = numbers.map(num => num); // Create a new array to trigger reactivity
+      // Distribute the pasted characters into the fields
+      otpCode.forEach((_, index) => {
+          if (index < numbers.length) {
+              otpCode[index] = numbers[index];
+              if (otpInputs[index]) {
+                  otpInputs[index].value = numbers[index]; // Ensure UI updates
+              }
+          }
+      });
 
-            // Set a slight delay to ensure the DOM updates before submitting
-            setTimeout(() => {
-                submitOTP();
-            }, 0);
-        }
-    };
+      // Check if the pasted content fills all fields and if so, submit
+      if (numbers.length === otpLength) {
+          setTimeout(submitOTP, 10);
+      }
+  };
 
   const handleInput = (index, event) => {
-    if(event.target.value.length == 6){
-      const numbers = event.target.value.split('').filter(char => !isNaN(char) && char !== ' ').slice(0, otpLength);
-
-        if (numbers.length === otpLength) {
-            otpCode = numbers.map(num => num); // Create a new array to trigger reactivity
-
-            // Set a slight delay to ensure the DOM updates before submitting
-            setTimeout(() => {
-                submitOTP();
-            }, 0);
-        }
-    } else {
       otpCode[index] = event.target.value.slice(0, 1);
-    }
-      submitOTP();
-  };
 
+      // Focus next input and submit only if all fields are filled
+      if (index < otpLength - 1) {
+          focusNextInput(index, event);
+      }
+      if (otpCode.every(num => num)) {
+          submitOTP();
+
+      }
+  };
 </script>
 
 <div class="row row-cards d-flex justify-content-center" on:paste={handlePaste}>
-    {#each Array(otpLength) as _, index}
-        <input
-            bind:this={otpInputs[index]}
-            type="text"
-            inputmode="numeric"
-            class="form-control qr-code"
-            maxlength="6"
-            pattern="[0-9]*"
-            name="otp"
-            autocomplete="one-time-code"
-            disabled={submitting}
-            value={otpCode[index]}
-            on:keyup={event => focusNextInput(index, event)}
-            on:input={event => handleInput(index, event)}
-        />
-    {/each}
-  </div>
+  {#each otpCode as _, index (index)}
+      <input
+          bind:this={otpInputs[index]}
+          type="text"
+          inputmode="numeric"
+          class="form-control qr-code"
+          maxlength="1"
+          pattern="[0-9]*"
+          name={`otp-${index}`}
+          autocomplete="one-time-code"
+          disabled={submitting}
+          bind:value={otpCode[index]}
+          on:keyup={event => focusNextInput(index, event)}
+          on:input={event => handleInput(index, event)}
+      />
+  {/each}
+</div>
 
 <style>
-    .qr-code {
-        width: 3em;
-        margin-right: 0.5em;
-        text-align: center;
-    }
+  .qr-code {
+      width: 3em;
+      margin-right: 0.5em;
+      text-align: center;
+  }
 </style>
