e687f850bffd6d100a9940cf4fc30782c151eb54
Jonas Bo Grimsgaard <jonas.bo.grimsgaard@nhn.no>
2024-08-16 11:44:20 +0000

    Closes #47

--- web/src/routes/(app)/+page.svelte
+++ web/src/routes/(app)/+page.svelte
@@ -84,7 +84,7 @@
 									><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M12 5l0 14"
 									></path><path d="M5 12l14 0"></path></svg
                   >
-                  Create new vulnerability</a
+                  Register new vulnerability</a
                   >
 									{/if}
                 </div>
